81ce17aa744c1c0800afff945c1b4a76b0efaf8a
Nicitaa <39565703+nicitaacom@users.noreply.github.com>
2023-09-05 13:04:00 +0000

    modal animation fixed

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,5 @@ import { BrowserRouter } from "react-router-dom"
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 )
