c41d57d97c6dfbaf0c3a63e7ea86df20ebab6f30
Nicitaa <39565703+nicitaacom@users.noreply.github.com>
2023-09-22 19:20:18 +0000

    pnpm in devDependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
 import { Layout } from "./components/Layout"
 import { Navbar } from "./components/Navbar/Navbar"
-
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: '23 store',
@@ -18,7 +14,7 @@ export default function RootLayout({children}:{children: React.ReactNode}) {
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar/>
         <Layout>
          {children} 
