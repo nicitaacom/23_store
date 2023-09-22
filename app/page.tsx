adf46cfad93c729a615ebde34069a230ccbd1999
Nicitaa <39565703+nicitaacom@users.noreply.github.com>
2023-09-22 11:54:13 +0000

    env security and intellisense for env

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export function Home() {
+ 
+  return (
+    <div className="text-2xl text-white flex flex-col gap-y-8 justify-between items-center py-12">
+      <section className="flex flex-col gap-y-4">
+        
+      </section>
+    </div>
+  )
+}
