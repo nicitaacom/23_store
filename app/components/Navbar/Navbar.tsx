aa54e408e90955fccfbe0645043865af7c5e58b3
Nicitaa <39565703+nicitaacom@users.noreply.github.com>
2023-09-26 15:59:04 +0000

    Navbar UI

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,3 +1,7 @@
+import { LuShoppingCart } from "react-icons/lu"
+import { FiPhoneCall } from "react-icons/fi"
+import { BiUserCircle } from "react-icons/bi"
+
 import { Switch } from ".."
 import { Language } from "../Language"
 import { HamburgerMenu, Logo, NavbarSearch } from "./components"
@@ -7,17 +11,25 @@ export default function Navbar() {
   return (
     <NavbarWrapper>
       <div className="flex flex-row gap-x-4 items-center">
-        {/* HAMBURGER-ICON */}
+        {/* HAMBURGER-ICON + LOGO */}
         <HamburgerMenu />
-
         <Logo />
+      </div>
+      {/* SEARCH + LANGUAGE */}
+      <div className="flex flex-row gap-x-2">
+        <NavbarSearch />
+        <Language className="hidden laptop:flex" />
+      </div>
 
+      {/* ICONS HELP */}
+      <div className="hidden mobile:flex flex-row gap-x-2 items-center ">
         <Switch />
-
-        <div className="flex flex-row gap-x-2">
-          <NavbarSearch/>
-          <Language/>
-        </div>
+        <LuShoppingCart className="cursor-pointer" size={28} />
+        <FiPhoneCall
+          className="cursor-pointer text-icon-color hover:brightness-75 transition-all duration-300 mr-2"
+          size={28}
+        />
+        <BiUserCircle className="cursor-pointer text-title hover:brightness-75 transition-all duration-300" size={32} />
       </div>
     </NavbarWrapper>
   )
