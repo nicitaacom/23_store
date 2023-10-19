f74f90062a155517af20acbd7af64f87cdbbb87f
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-19 17:41:37 +0200

    skeleton for initial page loading

--- app/components/Skeletons/InitialPageLoadingSkeleton.tsx
+++ app/components/Skeletons/InitialPageLoadingSkeleton.tsx
@@ -1,14 +1,14 @@
 import Skeleton from "react-loading-skeleton"
 import "react-loading-skeleton/dist/skeleton.css"
+import { NavbarSkeleton } from "./InitialPageLoading/NavbarSkeleton"
+import { ProductsSkeleton } from "./InitialPageLoading/ProductsSkeleton"
 
 export function InitialPageLoadingSkeleton() {
   return (
-    <div className="inset-0 z-[99] bg-[#121212] h-[100vh]" id="initial-loading">
-      <Skeleton
-        style={{ width: "80vw", height: "100%" }}
-        baseColor="hsl(var(--foreground))"
-        highlightColor="hsl(var(--foreground-hover))"
-      />
+    //pr-[17px] - for scrollbar needed - so skeleton looks closer to actual UI
+    <div className="inset-0 z-[99] bg-[#121212] h-[100vh] pr-[17px] overflow-hidden" id="initial-loading">
+      <NavbarSkeleton />
+      <ProductsSkeleton />
     </div>
   )
 }
