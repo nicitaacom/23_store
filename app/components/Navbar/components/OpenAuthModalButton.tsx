14ed8abd150c46c2a3b72081ebcea8bcc416f751
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-20 15:51:39 +0200

    aria label added for accessibility for gps

--- app/components/Navbar/components/OpenAuthModalButton.tsx
+++ app/components/Navbar/components/OpenAuthModalButton.tsx
@@ -10,7 +10,7 @@ export default function OpenAuthModalButton() {
   const updatedPath = pathname + (pathname.includes("?") ? "&" : "?") + "modal=" + "AuthModal&variant=login"
 
   return (
-    <Link className="w-fit" href={updatedPath}>
+    <Link className="w-fit" href={updatedPath} aria-label="login">
       <BiUserCircle className="cursor-pointer text-title hover:brightness-75 transition-all duration-300" size={32} />
     </Link>
   )
