e5c110feeffbac4e8801286c5ce74d016dfbd477
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-22 20:26:04 +0200

    tailwind classes for cart icon added

--- app/components/Navbar/components/OpenCartModalButton.tsx
+++ app/components/Navbar/components/OpenCartModalButton.tsx
@@ -14,9 +14,19 @@ export default function OpenCartModalButton() {
   const cartQuantity = cartStore.getCartQuantity()
 
   return (
-    <Link href={updatedPath} aria-label="cart">
+    <Link
+      className="mr-1 cursor-pointer text-title transition-all duration-300
+        before:absolute before:w-[20px] before:h-[20px] before:bg-brand before:rounded-full before:text-title-foreground
+        before:translate-x-[80%] before:translate-y-[-20%] before:z-[9]"
+      href={updatedPath}
+      aria-label="cart">
       <LuShoppingCart className="cursor-pointer hover:brightness-75 duration-300" size={28} />
-      {cartQuantity}
+      <div
+        className={`absolute min-w-[20px] translate-x-[80%] translate-y-[-175%] laptop:translate-y-[-155%]
+          flex justify-center text-center text-title-foreground 
+          text-[12px] laptop:text-[14px] z-[9] ${cartQuantity === 0 ? "hidden" : "flex"}`}>
+        {cartQuantity}
+      </div>
     </Link>
   )
 }
