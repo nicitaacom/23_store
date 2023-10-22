d9ec1100fcd0d07cd39f108c0f3e440bdcd5fdfc
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-22 17:27:22 +0200

    old and unused files deleted

--- app/(site)/getAnonymousCart.ts
+++
@@ -1,7 +0,0 @@
-import useAnonymousCartStore from "@/store/user/cartStore"
-
-export function getAnonymousCart() {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const anonymousCart = useAnonymousCartStore()
-  return anonymousCart.cartProducts
-}
