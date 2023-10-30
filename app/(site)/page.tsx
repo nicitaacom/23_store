22c0841956eb324ad8236a363665564c19274d62
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-10-30 16:30:54 +0100

    UX improved + responsive
    Now use can find Pagination controls in 1 place that not jumping up and down depending on how much products on a screen

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -26,11 +26,11 @@ export default async function Home({
   const entries = products.slice(start, end)
 
   return (
-    <div className="max-w-[1024px] text-2xl text-white flex flex-col gap-y-8 justify-between items-center py-12 mx-auto">
+    <div className="max-w-[1024px] text-2xl text-white flex flex-col gap-y-8 justify-between items-center py-12 min-h-[calc(100vh-3rem)] mx-auto">
       <section className="flex flex-col gap-y-4">
         <Products products={entries} />
       </section>
-      <div className="flex flex-row gap-x-4 justify-between items-center">
+      <div className="flex flex-col-reverse tablet:flex-row gap-4 justify-between items-center">
         <PaginationControls
           hasNextPage={end < totalItems}
           hasPrevPage={start > 0}
