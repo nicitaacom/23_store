21b783f605b4cab8d52f3a50c0171ddce46fb4fa
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-02 11:33:23 +0100

    form height overflow fix
    Now I always see title and <ul> add  / edit /delete product - not depending on how small device height is

--- app/components/ui/Modals/AdminPanel/AdminPanelModal.tsx
+++ app/components/ui/Modals/AdminPanel/AdminPanelModal.tsx
@@ -127,46 +127,49 @@ export function AdminPanelModal({ label, ownerProducts }: AdminPanelModalProps) 
 
   return (
     <ModalContainer
-      className={`w-[100vw] max-w-[768px] tablet:max-w-[650px] 
-     bg-primary rounded-md border-[1px] border-solid border-border-color pt-8 `}
+      className={twMerge(
+        `w-[100vw] max-w-[768px] max-h-full
+      flex flex-col bg-primary rounded-md border-[1px] border-solid border-border-color pt-8`,
+        productAction === "Add product" && "tablet:max-w-[650px]",
+        productAction === "Edit product" && "tablet:max-w-full laptop:max-w-[1024px]",
+      )}
       modalQuery="AdminPanel">
+      <h1 className="min-h-[40px] text-4xl text-center whitespace-nowrap mb-8">{label}</h1>
+      <ul className="min-h-[144px] tablet:min-h-[50px] flex flex-col tablet:flex-row justify-center mb-8">
+        <li>
+          <RadioButton
+            label="Add product"
+            inputName="product"
+            onChange={e => setProductAction(e.target.value)}
+            defaultChecked>
+            <div className="flex flex-row gap-x-2 items-center">
+              Add product <AiOutlinePlus className="text-success" />
+            </div>
+          </RadioButton>
+        </li>
+        <li>
+          <RadioButton label="Edit product" inputName="product" onChange={e => setProductAction(e.target.value)}>
+            <div className="flex flex-row gap-x-2 items-center">
+              Edit product <CiEdit className="text-warning" />
+            </div>
+          </RadioButton>
+        </li>
+        <li>
+          <RadioButton label="Delete product" inputName="product" onChange={e => setProductAction(e.target.value)}>
+            <div className="flex flex-row gap-x-2 items-center">
+              Delete product <AiOutlineDelete className="text-danger" />
+            </div>
+          </RadioButton>
+        </li>
+      </ul>
       <div
         className={twMerge(
           `relative w-full pb-8 flex flex-col items-center transition-all duration-500`,
           isDraggingg ? "overflow-hidden" : "overflow-y-scroll",
           productAction === "Add product" && "tablet:max-h-[900px]",
-          productAction === "Edit product" && "tablet:max-h-[800px]",
+          productAction === "Edit product" && "tablet:max-h-[600px]",
           productAction === "Delete product" && "h-[40vh] tablet:max-h-[400px]",
         )}>
-        <h1 className="text-4xl text-center whitespace-nowrap mb-8">{label}</h1>
-        <ul className="flex flex-col tablet:flex-row justify-center mb-8">
-          <li>
-            <RadioButton
-              label="Add product"
-              inputName="product"
-              onChange={e => setProductAction(e.target.value)}
-              defaultChecked>
-              <div className="flex flex-row gap-x-2 items-center">
-                Add product <AiOutlinePlus className="text-success" />
-              </div>
-            </RadioButton>
-          </li>
-          <li>
-            <RadioButton label="Edit product" inputName="product" onChange={e => setProductAction(e.target.value)}>
-              <div className="flex flex-row gap-x-2 items-center">
-                Edit product <CiEdit className="text-warning" />
-              </div>
-            </RadioButton>
-          </li>
-          <li>
-            <RadioButton label="Delete product" inputName="product" onChange={e => setProductAction(e.target.value)}>
-              <div className="flex flex-row gap-x-2 items-center">
-                Delete product <AiOutlineDelete className="text-danger" />
-              </div>
-            </RadioButton>
-          </li>
-        </ul>
-
         {/* ADD PRODUCT */}
 
         {productAction === "Add product" && (
--- app/components/ui/Modals/AdminPanel/components/OwnerProductImage.tsx
+++ app/components/ui/Modals/AdminPanel/components/OwnerProductImage.tsx
@@ -6,9 +6,9 @@ interface OwnerProductImageProps {
 
 export function OwnerProductImage({ imgUrl }: OwnerProductImageProps) {
   return (
-    <figure className="relative tablet:aspect-video tablet:h-[100px] tablet:w-fit object-coverw">
+    <figure className="relative tablet:aspect-video tablet:h-[100px] tablet:w-fit laptop:h-[150px] object-coverw">
       <Image
-        className="tablet:aspect-video w-full tablet:h-[100px] tablet:w-fit object-cover"
+        className="tablet:aspect-video w-full tablet:h-[100px] tablet:w-fit laptop:h-[150px] object-cover"
         src={imgUrl}
         alt="image"
         width={177}
