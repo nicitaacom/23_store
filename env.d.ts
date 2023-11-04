8214b1a5a2258be12864bb77d186d683dfa8e54e
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-04 12:53:41 +0100

    initialize stripe
    I do this because Antonio do the same
    https://youtu.be/2aeMRB8LL4o?t=18239
    
    Also my old approach https://i.imgur.com/ZGSw9qH.png
    not correct

--- env.d.ts
+++ env.d.ts
@@ -11,8 +11,8 @@ declare global {
       NEXT_PUBLIC_SUPABASE_ANON_KEY: string
       SUPABASE_SERVICE_ROLE_KEY: string
 
-      NEXT_PUBLIC_STRIPE_PUBLIC: string
-      NEXT_STRIPE_SECRET: string
+      NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: string
+      NEXT_STRIPE_SECRET_KEY: string
 
       NEXT_RESEND_PUBLIC: string
 
