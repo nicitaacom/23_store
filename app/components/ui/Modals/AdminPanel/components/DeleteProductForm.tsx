12b77cea76cbdc77c553e1ce05b74242d35968c1
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-05 13:43:32 +0100

    added dark and light mode if no product to delete

--- app/components/ui/Modals/AdminPanel/components/DeleteProductForm.tsx
+++ app/components/ui/Modals/AdminPanel/components/DeleteProductForm.tsx
@@ -1,12 +1,17 @@
+"use client"
+
 import { IDBProduct } from "@/interfaces/IDBProduct"
 import { OwnerDeleteProduct } from "./OwnerDeleteProduct"
 import Image from "next/image"
+import useDarkMode from "@/store/ui/darkModeStore"
 
 interface DeleteProductForm {
   ownerProducts: IDBProduct[]
 }
 
 export function DeleteProductForm({ ownerProducts }: DeleteProductForm) {
+  const isDarkMode = useDarkMode().isDarkMode
+
   return (
     <div className="w-[90%] h-full mx-auto">
       {ownerProducts.length > 0 ? (
@@ -17,7 +22,12 @@ export function DeleteProductForm({ ownerProducts }: DeleteProductForm) {
         </div>
       ) : (
         <div className="h-full flex flex-col gap-y-8 justify-center items-center pb-16 w-[90%] mx-auto">
-          <Image src="/no-products-to-delete.png" alt="no-products-to-delete.png" width={256} height={256} />
+          <Image
+            src={isDarkMode ? "/no-products-to-delete-dark.png" : "/no-products-to-delete-light.png"}
+            alt="no-products-to-delete.png"
+            width={256}
+            height={256}
+          />
           <h1 className="text-2xl text-center font-bold">You have no products to delete</h1>
         </div>
       )}
