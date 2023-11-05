391d6df9dc83da71bcf537ff60f1f229f5154eed
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-05 13:57:49 +0100

    added image if no products to edit
    with dark and light mode

--- app/components/ui/Modals/AdminPanel/components/EditProductForm.tsx
+++ app/components/ui/Modals/AdminPanel/components/EditProductForm.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import { useRouter } from "next/navigation"
+import Image from "next/image"
 
 import { IDBProduct } from "@/interfaces/IDBProduct"
+import useDarkMode from "@/store/ui/darkModeStore"
 import { OwnerProduct } from "./OwnerProduct"
 
 interface EditProductForm {
@@ -10,16 +11,26 @@ interface EditProductForm {
 }
 
 export function EditProductForm({ ownerProducts }: EditProductForm) {
+  const isDarkMode = useDarkMode().isDarkMode
+
   return (
-    <div className="w-[90%] mx-auto">
+    <div className="w-[90%] h-full mx-auto">
       {ownerProducts.length > 0 ? (
-        <div className="flex flex-col gap-y-4">
+        <div className="h-full flex flex-col gap-y-4">
           {ownerProducts.map(ownerProduct => (
             <OwnerProduct {...ownerProduct} key={ownerProduct.id} />
           ))}
         </div>
       ) : (
-        <h1 className="pt-24 text-2xl text-center font-bold w-[90%] mx-auto">You have no products to edit</h1>
+        <div className="h-full flex flex-col gap-y-8 justify-center items-center pb-16 w-[90%] mx-auto">
+          <Image
+            src={isDarkMode ? "/no-products-to-edit-dark.png" : "/no-products-to-edit-light.png"}
+            alt="no-products-to-edit.png"
+            width={256}
+            height={256}
+          />
+          <h1 className="text-2xl text-center font-bold">You have no products to edit</h1>
+        </div>
       )}
     </div>
   )
