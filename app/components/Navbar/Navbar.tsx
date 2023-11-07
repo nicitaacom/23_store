2223c508d1ba07400d61d438f859bed5f7331da2
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-07 15:37:33 +0100

    SupportButton incapsulated

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,11 +1,7 @@
-import Link from "next/link"
-import { FiPhoneCall } from "react-icons/fi"
 import { BiSearchAlt } from "react-icons/bi"
 
 import supabaseServer from "@/libs/supabaseServer"
-import { contact } from "@/constant/contacts"
 import { Language } from "../Language"
-import { DropdownContainer } from "../ui/DropdownContainer"
 import { SwitchDarkMode } from ".."
 import { NavbarWrapper } from "./components/NavbarWrapper"
 import {
@@ -17,6 +13,7 @@ import {
   OpenUserMenuButton,
 } from "./components"
 import { CtrlKBadge } from "./components/CtrlKBadge"
+import { SupportButton } from "./components/SupportButton"
 
 export default async function Navbar() {
   const {
@@ -43,23 +40,7 @@ export default async function Navbar() {
         <SwitchDarkMode className="max-[500px]:hidden" />
         <BiSearchAlt className="flex tablet:hidden" size={28} />
         <OpenCartModalButton />
-        <DropdownContainer
-          classNameDropdownContainer="hidden mobile:flex"
-          className="before:translate-x-[-300%] translate-x-[35%] w-[125px]"
-          icon={<FiPhoneCall size={28} />}>
-          <div className="flex flex-col gap-y-2 justify-center items-center px-4 py-2">
-            <div className="flex flex-col justify-center items-center">
-              <Link
-                className="hover:text-brand text-title text-center duration-300"
-                href={contact.telegram}
-                target="_blank"
-                rel="preload">
-                Telegram
-              </Link>
-              <p className="whitespace-nowrap">(response 8s)</p>
-            </div>
-          </div>
-        </DropdownContainer>
+        <SupportButton />
         {user ? <OpenUserMenuButton /> : <OpenAuthModalButton />}
       </div>
     </NavbarWrapper>
