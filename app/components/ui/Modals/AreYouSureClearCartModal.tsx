ec1b489e45a2ec318c269f2e7db7143f48856928
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-08 13:48:10 +0100

    feat: fix clear cart button

--- app/components/ui/Modals/AreYouSureClearCartModal.tsx
+++ app/components/ui/Modals/AreYouSureClearCartModal.tsx
@@ -4,17 +4,22 @@ import { useAreYouSureClearCartModal } from "@/store/ui/areYouSureClearCartModal
 import { AreYouSureModalContainer } from "./ModalContainers/AreYouSureModalContainer"
 import useCartStore from "@/store/user/cartStore"
 
-export function AreYouSureClearCartButton() {
+export function AreYouSureClearCartModal() {
   const areYouSuteClearCartModal = useAreYouSureClearCartModal()
 
   const cartStore = useCartStore()
+
+  function clearCart() {
+    cartStore.clearCart()
+    areYouSuteClearCartModal.closeModal()
+  }
 
   return (
     <AreYouSureModalContainer
       isOpen={areYouSuteClearCartModal.isOpen}
       label={"Are you sure you want to clear cart?"}
       primaryButtonVariant="danger"
-      primaryButtonAction={cartStore.clearCart}
+      primaryButtonAction={clearCart}
       primaryButtonLabel="Delete"
       secondaryButtonAction={areYouSuteClearCartModal.closeModal}
       secondaryButtonLabel="Back"
--- app/providers/ModalsProvider.tsx
+++ app/providers/ModalsProvider.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 
 import { AreYouSureDeleteProductModal } from "@/components/ui/Modals/AreYouSureDeleteProductModal"
 import { CtrlKModal } from "@/components/ui/Modals/CtrlKModal"
+import { AreYouSureClearCartModal } from "@/components/ui/Modals"
 
 //This provider uses only for modals based on useState
 export function ModalsProvider() {
@@ -19,7 +20,7 @@ export function ModalsProvider() {
 
   return (
     <>
-      {/* <AreYouSureClearCartModal/> */}
+      <AreYouSureClearCartModal />
       <AreYouSureDeleteProductModal />
       <CtrlKModal />
     </>
