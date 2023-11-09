63a10bd9a0e17065926a5dd1ff4a41821cb53ca6
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-09 19:46:59 +0100

    chore: isMounted deleted
    I deleted this because it not necessary and has no effect
    
    Also I decided to use areYouSureDeleteProduct modal instead of destructuring objects

--- app/components/ui/Modals/AreYouSureDeleteProductModal.tsx
+++ app/components/ui/Modals/AreYouSureDeleteProductModal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useRouter } from "next/navigation"
 import { BiTrash } from "react-icons/bi"
 import axios from "axios"
@@ -11,25 +11,14 @@ import useCartStore from "@/store/user/cartStore"
 
 export function AreYouSureDeleteProductModal() {
   const router = useRouter()
-  const areYouSureDeleteProductModal = useAreYouSureDeleteProductModal()
   const cartStore = useCartStore()
-  const { title, id } = useAreYouSureDeleteProductModal()
   const [isLoading, setIsLoading] = useState(false)
-  const [isMounted, setIsMounted] = useState(false)
-
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
-  // to prevent SSR because if I render this modal I trigger useEffect
-  // that trigger document.addEventListener on esc (I need close ctrlK modal on esc press)
-  if (!isMounted) {
-    return null
-  }
+  const areYouSureDeleteProductModal = useAreYouSureDeleteProductModal()
 
   async function deleteProduct() {
     setIsLoading(true)
     //archive product on stripe first and then in DB
-    await axios.post("/api/products/delete", { id: id })
+    await axios.post("/api/products/delete", { id: areYouSureDeleteProductModal.id })
 
     //close modal and refresh - so user immediately see changes
     areYouSureDeleteProductModal.closeModal()
@@ -44,14 +33,14 @@ export function AreYouSureDeleteProductModal() {
       isLoading={isLoading}
       label={
         <h2>
-          Are you sure you want delete <b>{title}</b>?
+          Are you sure you want delete <b>{areYouSureDeleteProductModal.title}</b>?
         </h2>
       }
       primaryButtonIcon={BiTrash}
       primaryButtonVariant="danger"
       primaryButtonAction={deleteProduct}
       primaryButtonLabel="Delete"
-      secondaryButtonAction={areYouSureDeleteProductModal.closeModal}
+      secondaryButtonAction={() => areYouSureDeleteProductModal.closeModal()}
       secondaryButtonLabel="Back"
     />
   )
