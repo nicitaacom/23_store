2108f36775d9ded9b986c64bb77d35aabfa16ada
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-09 19:34:12 +0100

    chore: second ModalQueryContainer deleted
    I deleted this because I had 2 ModalQueryContainer.tsx in my project - I don't need it
    
    I need only 1 ModalQueryContainer.tsx

--- app/components/ui/index.ts
+++ app/components/ui/index.ts
@@ -1,9 +1,8 @@
 import { Button } from "./Button"
 import { DropdownContainer } from "./DropdownContainer"
 import { DropdownItem } from "./DropdownItem"
-import { ModalQueryContainer } from "./Modals/ModalContainers/ModalQueryContainer"
 import { Checkbox } from "./Checkbox"
 import { Slider } from "./Slider"
 import { RadioButton } from "./RadioButton"
 
-export { Button, DropdownContainer, DropdownItem, ModalQueryContainer, Checkbox, Slider, RadioButton }
+export { Button, DropdownContainer, DropdownItem, Checkbox, Slider, RadioButton }
