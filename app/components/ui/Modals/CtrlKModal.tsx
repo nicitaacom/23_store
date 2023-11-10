c03c37512541340fade43cd1d2c7bc41aca151f2
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-10 10:31:47 +0100

    style: input w-full

--- app/components/ui/Modals/CtrlKModal.tsx
+++ app/components/ui/Modals/CtrlKModal.tsx
@@ -9,15 +9,13 @@ export function CtrlKModal() {
   const ctrlKModal = useCtrlKModal()
 
   return (
-    <ModalContainer className="w-full max-w-[450px] py-4" isOpen={ctrlKModal.isOpen} onClose={ctrlKModal.closeModal}>
+    <ModalContainer
+      className="relative w-full max-w-[450px] py-4"
+      isOpen={ctrlKModal.isOpen}
+      onClose={ctrlKModal.closeModal}>
       <div className="flex flex-col gap-y-2">
-        <h1>Search for products</h1>
-        <SearchInput
-          className="hidden tablet:flex w-[40vw] max-w-[600px]"
-          startIcon={<BiSearchAlt size={24} />}
-          name="searchQuery"
-          placeholder="Search..."
-        />
+        <h1 className="flex justify-center">Search for products</h1>
+        <SearchInput className="" startIcon={<BiSearchAlt size={24} />} name="searchQuery" placeholder="Search..." />
       </div>
     </ModalContainer>
   )
