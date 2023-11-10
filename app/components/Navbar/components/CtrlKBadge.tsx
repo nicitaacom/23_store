a01e15020521c2f1b23b756f5fef4cbc153bc7d6
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-10 10:51:26 +0100

    feat: don't open CtrlKModal if params
    I added this feature - so user don't open modal to search something if he already has opened modal

--- app/components/Navbar/components/CtrlKBadge.tsx
+++ app/components/Navbar/components/CtrlKBadge.tsx
@@ -1,14 +1,17 @@
 "use client"
 
 import { useCtrlKModal } from "@/store/ui/ctrlKModal"
+import { useSearchParams } from "next/navigation"
 import { useEffect } from "react"
 
 export function CtrlKBadge() {
   const ctrlKModal = useCtrlKModal()
+  const params = useSearchParams().get("modal")
+  console.log("searchParams - ", params)
 
   useEffect(() => {
     const handleKeydown = (e: KeyboardEvent) => {
-      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey) && !params) {
         //to prevent focus state on browser searchbar
         e.preventDefault()
         ctrlKModal.toggle()
@@ -16,7 +19,7 @@ export function CtrlKBadge() {
     }
     document.addEventListener("keydown", handleKeydown)
     return () => document.removeEventListener("keydown", handleKeydown)
-  }, [ctrlKModal, ctrlKModal.toggle])
+  }, [ctrlKModal, ctrlKModal.toggle, params])
 
   return <div className="bg-icon-color rounded inline-block text-title-foreground px-[4px] py-[2px[">⌘+K</div>
 }
