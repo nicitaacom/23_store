2c83fa2eeaa64d06802fa7c2578561bcef32941b
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-10 11:09:10 +0100

    feat: search functionality added
    Now my site has functionality to search with CtrkKModal - ctrl+k - enter query - press enter

--- app/components/ui/Modals/CtrlKModal.tsx
+++ app/components/ui/Modals/CtrlKModal.tsx
@@ -1,22 +1,34 @@
-"use client"
-
 import { BiSearchAlt } from "react-icons/bi"
 import { SearchInput } from "../Inputs/SearchInput"
 import { ModalContainer } from "./ModalContainers/"
 import { useCtrlKModal } from "@/store/ui/ctrlKModal"
+import { redirect } from "next/navigation"
 
 export function CtrlKModal() {
   const ctrlKModal = useCtrlKModal()
+
+  async function searchProducts(formData: FormData) {
+    const searchQuery = formData.get("searchQuery")?.toString()
+
+    ctrlKModal.closeModal()
+    if (searchQuery === "") {
+      redirect("/")
+    }
+
+    if (searchQuery) {
+      redirect("/search?query=" + searchQuery)
+    }
+  }
 
   return (
     <ModalContainer
       className="relative w-full max-w-[450px]"
       isOpen={ctrlKModal.isOpen}
       onClose={ctrlKModal.closeModal}>
-      <div className="flex flex-col gap-y-2">
+      <form action={searchProducts} className="flex flex-col gap-y-2">
         <h1 className="flex justify-center">Search for products</h1>
         <SearchInput startIcon={<BiSearchAlt size={24} />} name="searchQuery" placeholder="Search..." />
-      </div>
+      </form>
     </ModalContainer>
   )
 }
