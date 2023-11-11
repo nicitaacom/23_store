f4b471ba075e185bc3613aa1fa5ab340c137b628
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-11 19:29:00 +0100

    style: text-title for Input.tsx

--- app/components/ui/Inputs/Input.tsx
+++ app/components/ui/Inputs/Input.tsx
@@ -37,7 +37,7 @@ export function Input({
     <div className={`relative ${className}`}>
       <div className="absolute top-[50%] translate-y-[-50%] translate-x-[50%]">{startIcon}</div>
       <input
-        className={twMerge(`w-full rounded border-[1px] border-solid bg-transparent px-4 py-2 mb-1 outline-none 
+        className={twMerge(`w-full rounded border-[1px] border-solid bg-transparent px-4 py-2 mb-1 outline-none text-title 
         ${startIcon && "pl-10"}`)}
         type={type}
         inputMode={type === "number" ? "numeric" : undefined}
