f9351c2bceaa4286c80ff6c4737f5d8b7624ac58
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-11 19:25:19 +0100

    upd: function to setCookie added

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { cookies } from "next/headers"
+
 export const getURL = () => {
   let url = process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000"
 
@@ -5,4 +7,15 @@ export const getURL = () => {
   url = url.charAt(url.length - 1) === "/" ? url : `${url}/`
 
   return url
+}
+
+export function setCookie(name: string, val: string) {
+  const date = new Date()
+  const value = val
+
+  // Set it expire in 7 days
+  date.setTime(date.getTime() + 7 * 24 * 60 * 60 * 1000)
+
+  // Set it
+  document.cookie = name + "=" + value + "; expires=" + date.toUTCString() + "; path=/"
 }
