2115ccd449afe6682e20a23e9f8845819e69902a
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-11 19:28:43 +0100

    upd: added input
    I added input in SupportButton.tsx and also I accept in proct conversationId that I get on from SSR

--- app/components/SupportButton.tsx
+++ app/components/SupportButton.tsx
@@ -4,9 +4,23 @@ import { BiSupport } from "react-icons/bi"
 
 import { Button, DropdownContainer } from "./ui"
 import useSupportDropdownClose from "@/hooks/ui/useSupportDropdownClose"
+import { Input } from "./ui/Inputs"
+import { useState } from "react"
+import supabaseClient from "@/libs/supabaseClient"
+import useUserStore from "@/store/user/userStore"
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies"
 
-export function SupportButton() {
+export function SupportButton({ conversationId }: { conversationId: Promise<string | RequestCookie | undefined> }) {
   const { isDropdown, openDropdown, closeDropdown, toggle, supportDropdownRef } = useSupportDropdownClose()
+
+  const [userMessage, setUserMessage] = useState("")
+  const userStore = useUserStore()
+
+  const senderId = userStore.userId ? userStore.userId : `anonymous_${crypto.randomUUID()}`
+
+  async function sendMessage() {
+    const { data, error } = await supabaseClient.from("messages").insert({ body: userMessage, sender_id: senderId })
+  }
 
   //before:translate-y-[402px] should be +2px then <section className="h-[400px]
   //w-[400px] should be = section w-[400px]
@@ -26,8 +40,16 @@ export function SupportButton() {
           <BiSupport className="text-icon-color w-[24px] h-[24px] desktop:w-[32px] desktop:h-[32px]" />
         </Button>
       }>
-      <section className="h-[490px] w-[400px]">
-        <h1 className="text-center py-4 text-[1.4rem] font-semibold">Response ~15s</h1>
+      <section className="h-[490px] w-[400px] px-4 py-4 flex flex-col justify-between">
+        <h1 className="text-center text-[1.4rem] font-semibold">Response ~15s</h1>
+        <form className="flex flex-row gap-x-2 items-center" onSubmit={sendMessage}>
+          <Input
+            className="w-full"
+            value={userMessage}
+            onChange={e => setUserMessage(e.target.value)}
+            placeholder="Enter your message..."
+          />
+        </form>
       </section>
     </DropdownContainer>
   )
