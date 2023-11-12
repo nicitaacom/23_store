43cacaf524f0f71933392941beb409c10f6bb457
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-12 15:19:09 +0100

    fix: correct date with formatTime.ts

--- app/utils/formatTime.ts
+++ app/utils/formatTime.ts
@@ -3,13 +3,10 @@ export function formatTime(dateString: string, hideTimezone?: boolean): string {
 
   // Get the user's timezone
   const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone
-  const timezoneOffset = date.getTimezoneOffset() / 60 // Get the timezone offset in hours
-
-  // Adjust the time based on the timezone offset
-  date.setHours(date.getHours() + timezoneOffset)
 
   const hours = date.getHours().toString().padStart(2, "0") // Get hours in 2-digit format
   const minutes = date.getMinutes().toString().padStart(2, "0") // Get minutes in 2-digit format
+
   if (hideTimezone) {
     return `${hours}:${minutes}`
   } else {
