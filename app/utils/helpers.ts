100ee8b1aeda188b64733ef279b1face38f8aae3
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-16 08:44:44 +0100

    upd: helpers to getURL added

---
+++ app/utils/helpers.ts
@@ -0,0 +1,9 @@
+//This function may be user on client side and server side
+export const getURL = () => {
+  let url = process.env.NEXT_PUBLIC_SITE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000"
+
+  url = url.includes("http") ? url : `https://${url}`
+  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`
+
+  return url
+}
