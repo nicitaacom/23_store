096367b60ce3788d0bd8273abaabd24efce14c05
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-17 12:45:47 +0100

    chore: typo fixed 'default-outline'

--- app/components/SupportButton/SupportButton.tsx
+++ app/components/SupportButton/SupportButton.tsx
@@ -117,7 +117,7 @@ export function SupportButton({ conversationId, initialMessages }: SupportButton
       icon={
         <Button
           className="w-[48px] h-[48px] desktop:w-[64px] desktop:h-[64px] fixed bottom-4 right-6 rounded-full border border-border-color"
-          variant="outline">
+          variant="default-outline">
           <BiSupport className="text-icon-color w-[24px] h-[24px] desktop:w-[32px] desktop:h-[32px]" />
         </Button>
       }>
