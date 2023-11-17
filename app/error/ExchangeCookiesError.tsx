1327a999e6dd74d3487ec6b71ec36710e1413023
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-17 16:50:42 +0100

    upd: image for this error added

--- app/error/ExchangeCookiesError.tsx
+++ app/error/ExchangeCookiesError.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image"
+
+import useDarkMode from "@/store/ui/darkModeStore"
+import { BackToMainButton } from "./components/BackToMainButton"
+
 export function ExchangeCookiesError({ message }: { message?: string }) {
+  const { isDarkMode } = useDarkMode()
+
   return (
     <div className="flex flex-col gap-y-4 items-center justify-center">
-      <div className="h-[35vh] w-full bg-brand" />
+      <Image
+        src={
+          isDarkMode
+            ? "/errors/user-not-found-after-exchanging-cookies-dark.jpg"
+            : "/errors/user-not-found-after-exchanging-cookies-light.jpg"
+        }
+        alt="invalid-flow-state-found"
+        width={1920}
+        height={386}
+      />
       <p className="text-danger">{message ? message : "No user found when exchanging cookies"}</p>
-      <p>In short, there is a mistake here</p>
       <p>You might verified your email on new device or in incognito mode</p>
       <p>To get support contact us here - {process.env.NEXT_PUBLIC_SUPPORT_EMAIL}</p>
+      <BackToMainButton />
     </div>
   )
 }
