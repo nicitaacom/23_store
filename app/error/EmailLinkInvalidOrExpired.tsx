df3629d561b36f5e133dc76788b2937ab9349318
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-17 16:50:54 +0100

    upd: image for this error added

--- app/error/EmailLinkInvalidOrExpired.tsx
+++ app/error/EmailLinkInvalidOrExpired.tsx
@@ -1,11 +1,27 @@
+import Image from "next/image"
+
+import useDarkMode from "@/store/ui/darkModeStore"
+import { BackToMainButton } from "./components/BackToMainButton"
+
 export function EmailLinkInvalidOrExpired() {
+  const { isDarkMode } = useDarkMode()
+
   return (
     <div className="flex flex-col gap-y-4 items-center justify-center">
-      <div className="h-[35vh] w-full bg-brand" />
+      <Image
+        src={
+          isDarkMode
+            ? "/errors/email-link-invalid-or-has-expired-dark.jpg"
+            : "/errors/email-link-invalid-or-has-expired-light.jpg"
+        }
+        alt="invalid-flow-state-found"
+        width={1920}
+        height={386}
+      />
       <p className="text-danger">Email link is invalid or has expired</p>
-      <p>In short, there is a mistake here</p>
       <p>Try again and use link that you become ASAP</p>
       <p>Also don&apos;t use link that you already used</p>
+      <BackToMainButton />
     </div>
   )
 }
