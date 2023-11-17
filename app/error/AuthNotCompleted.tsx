e75e8870748878fdbe819e81e87b46ac25c57b24
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-17 16:51:36 +0100

    upd: image for this error added

--- app/error/AuthNotCompleted.tsx
+++ app/error/AuthNotCompleted.tsx
@@ -1,13 +1,23 @@
-"use client"
+import Image from "next/image"
+import { BackToMainButton } from "./components/BackToMainButton"
+import useDarkMode from "@/store/ui/darkModeStore"
 
 export function AuthNotCompleted() {
+  const { isDarkMode } = useDarkMode()
+
   return (
     <div className="flex flex-col gap-y-4 items-center justify-center">
-      <div className="h-[35vh] w-full bg-brand" />
+      <Image
+        className="w-full"
+        src={isDarkMode ? "/errors/invalid-flow-state-found-dark.jpg" : "/errors/invalid-flow-state-found-light.jpg"}
+        alt="invalid-flow-state-found"
+        width={1920}
+        height={386}
+      />
       <p className="text-danger">You auth flow not completed</p>
-      <p>In short, there is a mistake here</p>
       <p>You got this error because you entered url in searchbar that needs to success message if auth completed</p>
       <p>Now close this page</p>
+      <BackToMainButton />
     </div>
   )
 }
