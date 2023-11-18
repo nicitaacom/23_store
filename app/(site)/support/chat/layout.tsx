f2c615dd18640e2d2d463971adca0dcb36d72fe4
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-18 20:07:42 +0100

    feat: protected rote for SUPPORT and ADMIN

--- app/(site)/support/chat/layout.tsx
+++ app/(site)/support/chat/layout.tsx
@@ -1,5 +1,12 @@
+import supabaseServer from "@/libs/supabaseServer"
 import { Sidebar } from "./components/Sidebar"
+import { redirect } from "next/navigation"
 
-export default function SupportChatLayout({ children }: { children: React.ReactNode }) {
+export default async function SupportChatLayout({ children }: { children: React.ReactNode }) {
+  const { data: role_response } = await supabaseServer().from("users").select("role").single()
+
+  if (!["ADMIN", "SUPPORT"].includes(role_response?.role!)) {
+    redirect("/")
+  }
   return <Sidebar>{children}</Sidebar>
 }
