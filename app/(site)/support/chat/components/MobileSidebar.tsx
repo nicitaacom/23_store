76ff35ce1f243e5e7f252468905d421051000ab6
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-18 17:36:51 +0100

    feat: UI for mobile sidebar done

--- app/(site)/support/chat/components/MobileSidebar.tsx
+++ app/(site)/support/chat/components/MobileSidebar.tsx
@@ -1,18 +1,51 @@
 export function MobileSidebar() {
   return (
     <div className="block laptop:hidden h-full">
-      <nav className="flex flex-col gap-y-4 justify-center items-center">
-        <div className="w-full border border-border-color text-center px-4 py-2">Username</div>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
-        <p>item</p>
+      <nav className="flex flex-col gap-y-4 justify-center items-center px-16">
+        <div className="relative w-full border border-border-color text-center pl-4 pr-8 py-2">
+          <h3 className="font-semibold truncate">Username-extra-long</h3>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-100%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-120%] after:z-[9] after:content-['99']"
+          />
+        </div>
+        {/* TODO - create case if messages more then 99 - show 99 */}
+        <div className="relative w-full border border-border-color text-center pl-4 pr-8 py-2">
+          <h3 className="font-semibold">Username_long</h3>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-100%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-120%] after:z-[9] after:content-['99+']"
+          />
+        </div>
+        <div className="relative w-full border border-border-color text-center pl-4 pr-8 py-2">
+          <h3 className="font-semibold">Username</h3>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-100%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-120%] after:z-[9] after:content-['99']"
+          />
+        </div>
+        <div className="relative w-full border border-border-color text-center pl-4 pr-8 py-2">
+          <h3 className="font-semibold">Username</h3>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-100%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-120%] after:z-[9] after:content-['99']"
+          />
+        </div>
+        {/* UNREAD (sort - unread first) */}
+        <div className="w-full border border-border-color text-subTitle text-center px-4 py-2">Username</div>
+        <div className="w-full border border-border-color text-subTitle text-center px-4 py-2">Username</div>
+        <div className="w-full border border-border-color text-subTitle text-center px-4 py-2">Username</div>
+        <div className="w-full border border-border-color text-subTitle text-center px-4 py-2">Username</div>
+        <div className="w-full border border-border-color text-subTitle text-center px-4 py-2">Username</div>
+        <div className="w-full border border-border-color text-subTitle text-center px-4 py-2">Username</div>
       </nav>
     </div>
   )
