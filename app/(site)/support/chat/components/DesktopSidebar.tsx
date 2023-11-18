043550863f3e1dec09c719d4a0383abd12a81247
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-18 17:37:03 +0100

    feat: UI for desktop sidebar done

--- app/(site)/support/chat/components/DesktopSidebar.tsx
+++ app/(site)/support/chat/components/DesktopSidebar.tsx
@@ -1,7 +1,73 @@
 export function DesktopSidebar() {
   return (
-    <div className="hidden laptop:block absolute inset-y-0 shadow-md w-20 bg-foreground">
-      <nav className="flex flex-col">item</nav>
+    <div className="hidden laptop:block absolute inset-y-0 shadow-md w-64 bg-foreground">
+      <nav className="flex flex-col">
+        <div className="px-4 py-2 hover:bg-foreground-accent duration-150 cursor-pointer">
+          <h3 className="font-semibold">Username</h3>
+          <p className="text-sm">Last message </p>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-150%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-180%] after:z-[9] after:content-['99']"
+          />
+        </div>
+        <div className="px-4 py-2 hover:bg-foreground-accent duration-150 cursor-pointer">
+          <h3 className="font-semibold">Username</h3>
+          <p className="text-sm">Last message </p>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-150%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-180%] after:z-[9] after:content-['99']"
+          />
+        </div>
+        <div className="px-4 py-2 hover:bg-foreground-accent duration-150 cursor-pointer">
+          <h3 className="font-semibold">Username</h3>
+          <p className="text-sm">Last message </p>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-150%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-180%] after:z-[9] after:content-['99']"
+          />
+        </div>
+        <div className="px-4 py-2 hover:bg-foreground-accent duration-150 cursor-pointer">
+          <h3 className="font-semibold">Username</h3>
+          <p className="text-sm">Last message </p>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-150%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-180%] after:z-[9] after:content-['99']"
+          />
+        </div>
+        <div className="px-4 py-2 hover:bg-foreground-accent duration-150 cursor-pointer">
+          <h3 className="font-semibold">Username</h3>
+          <p className="text-sm">Last message </p>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-150%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-180%] after:z-[9] after:content-['99']"
+          />
+        </div>
+        <div className="px-4 py-2 hover:bg-foreground-accent duration-150 cursor-pointer">
+          <h3 className="font-semibold">Username</h3>
+          <p className="text-sm">Last message </p>
+          <div
+            className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
+        before:right-2 before:translate-y-[-150%] before:z-[9]
+        after:absolute after:w-[20px] after:h-[20px] after:text-title-foreground
+        after:right-2.5 after:translate-y-[-180%] after:z-[9] after:content-['99']"
+          />
+        </div>
+        <div className="px-4 py-2 hover:bg-foreground-accent duration-150 cursor-pointer">
+          <p>Username</p>
+          <p className="text-sm">Last message </p>
+          <div className="" />
+        </div>
+      </nav>
     </div>
   )
 }
