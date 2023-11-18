fd6bb0cd6ddd73e7694fa268320ac2089a987f3b
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-18 20:07:07 +0100

    upd: width instead of pl
    I do this because for me its better

--- app/(site)/support/chat/components/Sidebar.tsx
+++ app/(site)/support/chat/components/Sidebar.tsx
@@ -3,10 +3,10 @@ import { MobileSidebar } from "./MobileSidebar"
 
 export function Sidebar({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative h-[calc(100vh-64px)]">
+    <div className="relative h-[calc(100vh-64px)] flex">
       <DesktopSidebar />
       <MobileSidebar />
-      <main className="lg:pl-20 h-full">{children}</main>
+      <main className="w-[calc(100%-16rem)] h-full">{children}</main>
     </div>
   )
 }
