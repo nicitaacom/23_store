2e89ce49f8315cbfd0f4d2be60b5d4b2c66ac3a3
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-19 09:17:21 +0100

    chore: Sidebar renamed to ConversationList

--- app/(site)/support/chat/layout.tsx
+++ app/(site)/support/chat/layout.tsx
@@ -1,11 +1,11 @@
 import supabaseServer from "@/libs/supabaseServer"
-import { Sidebar } from "./components/Sidebar"
+import { ConversationsList } from "./components/ConversationsList"
 import { redirect } from "next/navigation"
 
 export default async function SupportChatLayout({ children }: { children: React.ReactNode }) {
   const { data: role_response } = await supabaseServer().from("users").select("role").single()
 
-  // boilerplate if you have more roles without aaccess to /support/chat
+  // boilerplate if you have more roles without access to /support/chat
   // Allow roles in array to visit this page
   // if (!["ADMIN", "SUPPORT"].includes(role_response?.role!)) {
   //   redirect("/")
@@ -15,5 +15,5 @@ export default async function SupportChatLayout({ children }: { children: React.
     redirect("/")
   }
 
-  return <Sidebar>{children}</Sidebar>
+  return <ConversationsList>{children}</ConversationsList>
 }
