4fcfcf93e9de7cdceaf7fcb92205346fe76935ba
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-19 11:59:08 +0100

    chore: chat renamed to ticket
    Because this name is more suitable for my case

--- app/(site)/support/tickets/[ticketId]/page.tsx
+++ app/(site)/support/tickets/[ticketId]/page.tsx
@@ -1,10 +1,10 @@
+// import getAllMessages from "@/actions/getAllMessages"
 import { MessageInput, MessagesBody, MessagesHeader } from "../components"
 
 export default async function ChatPage({ params }: { params: { chatId: string } }) {
-  // const messages = await getMessages(params.conversationId)
+  // const messages = await getAllMessagesFromConversation()
 
   // if !conversation (e.g 093jf0e) - return EmptyState
-  console.log(11, params.chatId)
 
   return (
     <div className="bg-foreground-accent w-full h-full flex flex-col justify-between items-center z-[100]">
--- app/(site)/support/tickets/components/MobileSidebar.tsx
+++ app/(site)/support/tickets/components/MobileSidebar.tsx
@@ -1,9 +1,11 @@
-export function MobileSidebar() {
+import { IConversation } from "@/interfaces/IConversation"
+
+export function MobileSidebar({ sender_username }: IConversation) {
   return (
     <aside className="block laptop:hidden w-full h-full">
       <nav className="flex flex-col gap-y-4 justify-center items-center px-16">
         <div className="relative w-full border border-border-color text-center pl-4 pr-8 py-2">
-          <h3 className="font-semibold truncate">Username-extra-long</h3>
+          <h3 className="font-semibold truncate">{sender_username}</h3>
           <div
             className="before:absolute before:w-[25px] before:h-[25px] before:bg-info before:rounded-full
         before:right-2 before:translate-y-[-100%] before:z-[9]
