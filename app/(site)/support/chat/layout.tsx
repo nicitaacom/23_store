a278880d0fb6e6f0f34dcd342e900f39e06def43
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-19 07:13:01 +0100

    upd: redirect role USER to /

--- app/(site)/support/chat/layout.tsx
+++ app/(site)/support/chat/layout.tsx
@@ -5,8 +5,15 @@ import { redirect } from "next/navigation"
 export default async function SupportChatLayout({ children }: { children: React.ReactNode }) {
   const { data: role_response } = await supabaseServer().from("users").select("role").single()
 
-  if (!["ADMIN", "SUPPORT"].includes(role_response?.role!)) {
+  // boilerplate if you have more roles without aaccess to /support/chat
+  // Allow roles in array to visit this page
+  // if (!["ADMIN", "SUPPORT"].includes(role_response?.role!)) {
+  //   redirect("/")
+  // }
+
+  if (role_response?.role === "USER") {
     redirect("/")
   }
+
   return <Sidebar>{children}</Sidebar>
 }
