2832540760a66360705c9c1cb6bc32910082771b
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-19 16:48:08 +0100

    upd: type renamed

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -3,29 +3,29 @@ import { pusherServer } from "@/libs/pusher"
 import supabaseAdmin from "@/libs/supabaseAdmin"
 import { NextResponse } from "next/server"
 
-export type TMessageRequest = {
+export type TAPIMessages = {
   body: string
-  conversationId: string
   senderId: string
+  senderUsername: string
+  ticketId: string
+  images?: string
 }
 
 export async function POST(req: Request) {
-  const { body, conversationId, senderId } = (await req.json()) as TMessageRequest
+  const { ticketId, body, images, senderId, senderUsername } = (await req.json()) as TAPIMessages
 
-  const newMessage: IMessage = {
-    created_at: new Date().toISOString(),
-    conversation_id: conversationId,
-    body: body,
+  const newMessage = {
     sender_id: senderId,
-    is_closed_conversation: false,
-    id: crypto.randomUUID(),
+    owner_username: senderUsername,
+    body: body,
+    images: images,
   }
   //TODO - updated converstaion in the future - https://youtu.be/PGPGcKBpAk8?t=28253
 
   //supabaseAdmin because I don't want any user with ANON_KEY abuse way to send messages (smsBomber protecting)
   //see more (use subtitles if needed) - https://www.youtube.com/watch?v=voy5_XGETMc&ab_channel=overbafer1
-  await supabaseAdmin.from("messages").insert(newMessage)
-  await pusherServer.trigger(conversationId, "messages:new", newMessage)
+  await supabaseAdmin.from("tickets").insert(newMessage)
+  await pusherServer.trigger(ticketId, "messages:new", newMessage)
 
   return NextResponse.json({ req })
 }
