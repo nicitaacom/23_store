7d5ef69946cf6a6c7009e32aa3bb30404f99e4d5
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-20 08:58:26 +0100

    fix: insert new message fixed
    Now it insert new message in DB properly

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,31 +1,33 @@
-import { IMessage } from "@/interfaces/IMessage"
 import { pusherServer } from "@/libs/pusher"
 import supabaseAdmin from "@/libs/supabaseAdmin"
 import { NextResponse } from "next/server"
 
 export type TAPIMessages = {
-  body: string
+  ticketId: string
   senderId: string
   senderUsername: string
-  ticketId: string
-  images?: string
+  body: string
+  images?: string[]
 }
 
 export async function POST(req: Request) {
   const { ticketId, body, images, senderId, senderUsername } = (await req.json()) as TAPIMessages
 
   const newMessage = {
+    ticket_id: ticketId,
     sender_id: senderId,
-    owner_username: senderUsername,
+    sender_username: senderUsername,
     body: body,
     images: images,
   }
+
   //TODO - updated converstaion in the future - https://youtu.be/PGPGcKBpAk8?t=28253
 
   //supabaseAdmin because I don't want any user with ANON_KEY abuse way to send messages (smsBomber protecting)
   //see more (use subtitles if needed) - https://www.youtube.com/watch?v=voy5_XGETMc&ab_channel=overbafer1
-  await supabaseAdmin.from("tickets").insert(newMessage)
+  const { error } = await supabaseAdmin.from("messages").insert(newMessage)
   await pusherServer.trigger(ticketId, "messages:new", newMessage)
+  if (error) console.log(29, "error insert newMessage - ", error)
 
   return NextResponse.json({ req })
 }
