ab93405df81e346adcdde541ea6fddb371c648ac
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-20 20:17:26 +0100

    +-upd: div changed to main

--- app/(site)/support/tickets/page.tsx
+++ app/(site)/support/tickets/page.tsx
@@ -1,9 +1,10 @@
 export default function SupportChatPage() {
   return (
-    <div
-      className="bg-foreground-accent w-full h-full flex justify-center items-center
+    <main
+      className="w-full h-full laptop:w-[calc(100%-16rem)] hidden laptop:flex bg-foreground-accent
+      justify-center items-center
     shadow-[inset_0px_8px_6px_rgba(0,0,0,0.4)] z-[100]">
       Select chat
-    </div>
+    </main>
   )
 }
