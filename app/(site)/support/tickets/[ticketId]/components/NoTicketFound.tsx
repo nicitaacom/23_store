2004dcaa492dc8146f286d0660f6484a1d185494
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-20 19:58:16 +0100

    +feat: 1024px- flex button BackToTickets.tsx
    I added this button so user can go back to tickets if no ticket with {ticketId} found
    I accept ticketId in props that comes from [ticketId]/page.tsx
    
    Also I added image to improve UX

--- app/(site)/support/tickets/[ticketId]/components/NoTicketFound.tsx
+++ app/(site)/support/tickets/[ticketId]/components/NoTicketFound.tsx
@@ -1,9 +1,27 @@
-export function NoTicketFound() {
+"use client"
+
+import Image from "next/image"
+import { BackToTickets } from "./BackToTickets"
+import useDarkMode from "@/store/ui/darkModeStore"
+
+export function NoTicketFound({ ticketId }: { ticketId: string }) {
+  const { isDarkMode } = useDarkMode()
+
   return (
     <div
-      className="bg-foreground-accent w-full h-full flex justify-center items-center
-    shadow-[inset_0px_8px_6px_rgba(0,0,0,0.4)] z-[100]">
-      No ticket with this id found
+      className="bg-foreground-accent w-full h-full flex flex-col gap-y-16 justify-center items-center
+    shadow-[inset_0px_8px_6px_rgba(0,0,0,0.4)] pb-8 z-[100]">
+      <Image
+        className="hidden mobile:block ml-6 w-[360px] h-[180px] laptop:w-[480px] laptop:h-[240px]"
+        src={isDarkMode ? "/no-ticket-with-ticketId-found-dark.png" : "/no-ticket-with-ticketId-found-light.png"}
+        alt="No ticket id found"
+        width={480}
+        height={240}
+      />
+      <div className="flex flex-col gap-y-2">
+        <h1 className="text-lg laptop:text-2xl">No ticket found with id {ticketId}</h1>
+        <BackToTickets />
+      </div>
     </div>
   )
 }
