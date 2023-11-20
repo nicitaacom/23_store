5e02baa534a123bcbade97a0719209ec88dafdfc
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-20 15:31:33 +0100

    fix: redirect anonymous user to /
    If anonymous user decide to visit protected route - I redirect him to /

--- app/(site)/support/tickets/layout.tsx
+++ app/(site)/support/tickets/layout.tsx
@@ -3,15 +3,15 @@ import { TicketsList } from "./components/TicketsList"
 import { redirect } from "next/navigation"
 
 export default async function SupportChatLayout({ children }: { children: React.ReactNode }) {
-  const { data: role_response } = await supabaseServer().from("users").select("role").single()
+  const { data: role_response, error: anonymous_user } = await supabaseServer().from("users").select("role").single()
 
-  // boilerplate if you have more roles without access to /support/chat
-  // Allow roles in array to visit this page
-  // if (!["ADMIN", "SUPPORT"].includes(role_response?.role!)) {
-  //   redirect("/")
-  // }
+  if (role_response?.role === "USER" || anonymous_user) {
+    // boilerplate if you have more roles without access to /support/chat
+    // Allow roles in array to visit this page
+    // if (!["ADMIN", "SUPPORT"].includes(role_response?.role!)) {
+    //   redirect("/")
+    // }
 
-  if (role_response?.role === "USER") {
     redirect("/")
   }
 
