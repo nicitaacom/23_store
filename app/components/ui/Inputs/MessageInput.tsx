4f3e93092dfedee01f4fe0cda6e37afbb5ead8f0
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-20 14:56:52 +0100

    chore: MessageInput moved to /ui/Inputs
    I did it because I re-use MessageInput 2 times
    1. In SupportButton.tsx
    2. In /support/tickets /[ticketId] - page.tsx

---
+++ app/components/ui/Inputs/MessageInput.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { Input } from "@/components/ui/Inputs"
+import { useState } from "react"
+import { FieldErrors, UseFormRegister } from "react-hook-form"
+import { twMerge } from "tailwind-merge"
+
+interface FormData {
+  message: string
+}
+
+interface MessageInputProps {
+  id: keyof FormData
+  register: UseFormRegister<FormData>
+  className?: string
+}
+
+export function MessageInput({ className, id, register }: MessageInputProps) {
+  return (
+    <div className="w-[calc(100%-2px)] bg-foreground-accent p-4">
+      <input
+        className={twMerge(
+          `w-full rounded border border-solid bg-transparent px-4 py-2 mb-1 outline-none text-title`,
+          className,
+        )}
+        id={id}
+        tabIndex={0}
+        placeholder="Enter message..."
+        {...register(id)}
+      />
+    </div>
+  )
+}
