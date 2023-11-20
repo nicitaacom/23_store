20518a8562c1998ebeec38a8ae509bad13cf357a
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-20 18:28:59 +0100

    fix: text centered

--- app/(site)/support/tickets/[ticketId]/components/NoTicketFound.tsx
+++ app/(site)/support/tickets/[ticketId]/components/NoTicketFound.tsx
@@ -1,7 +1,7 @@
 export function NoTicketFound() {
   return (
     <div
-      className="bg-foreground-accent w-full h-full justify-center items-center
+      className="bg-foreground-accent w-full h-full flex justify-center items-center
     shadow-[inset_0px_8px_6px_rgba(0,0,0,0.4)] z-[100]">
       No ticket with this id found
     </div>
