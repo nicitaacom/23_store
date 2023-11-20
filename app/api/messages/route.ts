dc4b8470f59fa5ac237003f89b08774f546750b0
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-20 15:15:21 +0100

    fix: insert current date-time
    I did it with help of chatGPT
    
    I need to insert date in supabase to get access about it in messages.map
    So I can see current date-time instead of NaN

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,5 +1,6 @@
 import { pusherServer } from "@/libs/pusher"
 import supabaseAdmin from "@/libs/supabaseAdmin"
+import { formatTime } from "@/utils/formatTime"
 import { NextResponse } from "next/server"
 
 export type TAPIMessages = {
@@ -13,14 +14,21 @@ export type TAPIMessages = {
 export async function POST(req: Request) {
   const { ticketId, body, images, senderId, senderUsername } = (await req.json()) as TAPIMessages
 
+  // dance arounding to insert current date-time
+  const now = new Date()
+  const timestampString = now.toISOString().replace("T", " ").replace("Z", "+00")
+
   const newMessage = {
     id: crypto.randomUUID(), // needed to set in cuurent pusher channel (for key prop in react)
+    created_at: timestampString,
     ticket_id: ticketId,
     sender_id: senderId,
     sender_username: senderUsername,
     body: body,
     images: images,
   }
+
+  console.log(26, "newMessage - ", newMessage)
 
   //TODO - updated converstaion in the future - https://youtu.be/PGPGcKBpAk8?t=28253
 
