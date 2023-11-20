56be51757501a0f6ffb886c5a6b8ee5fb9947151
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-20 08:59:39 +0100

    upd: get initial messages .eq anonymousId

--- app/actions/getInitialMessages.ts
+++ app/actions/getInitialMessages.ts
@@ -6,24 +6,18 @@ const getInitialMessages = async () => {
   //get userId to fetch messages by userId
   const {
     data: { user },
-    error: user_error,
   } = await supabaseServer().auth.getUser()
-
-  if (user_error) {
-    console.log(10, "userSessionError - ", user_error)
-    throw new Error("getTicketId error - ", user_error)
-  }
 
   const userId = user?.id === undefined ? getCookie("anonymousId")?.value : user.id
 
   // get messages by userId
-  const { data: messages, error } = await supabaseAdmin // because may be anonymousId that not matches RLS (sender_id = auth.uid())
+  const { data: messages, error: messages_error } = await supabaseAdmin // because may be anonymousId that not matches RLS (sender_id = auth.uid())
     .from("messages")
     .select("*")
     .eq("sender_id", userId as string)
-  if (error) {
-    console.log(22, "error - ", error)
-    throw error
+  if (messages_error) {
+    console.log(22, "messages_error - ", messages_error)
+    throw messages_error
   }
   if (messages && messages.length === 0) {
     return null
