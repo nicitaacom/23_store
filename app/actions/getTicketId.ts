b035709fe8dd7fe6f6db2cf2fb2e7e2606ecacaa
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-20 07:15:53 +0100

    upd: return null if !ticket_id

--- app/actions/getTicketId.ts
+++ app/actions/getTicketId.ts
@@ -2,35 +2,33 @@ import supabaseAdmin from "@/libs/supabaseAdmin"
 import supabaseServer from "@/libs/supabaseServer"
 import { getCookie } from "@/utils/helpersSSR"
 
+// I getTicketId to subscribe pusher to this channelId (ticketId) to get live-update
 const getTicketId = async () => {
-  const { data: userSessionData, error: userSessionError } = await supabaseServer().auth.getSession()
+  const {
+    data: { user },
+    error: user_user_error,
+  } = await supabaseServer().auth.getUser()
 
-  if (userSessionError) throw new Error("getTicketId error - ", userSessionError)
-
-  const userId =
-    userSessionData.session?.user.id === undefined ? getCookie("anonymousId")?.value : userSessionData.session?.user.id
+  const userId = user?.id === undefined ? getCookie("anonymousId")?.value : user.id
 
   try {
-    const { data: ticket_id, error } = await supabaseAdmin
+    // 1 user may have only 1 open ticket - that's why single()
+    const { data: ticket_id } = await supabaseAdmin
       .from("tickets")
       .select("id")
       .eq("owner_username", userId as string)
       .eq("is_open", true)
+      .single()
 
-    if (error) {
-      console.error("Error fetching ticketId - ", error)
+    if (ticket_id) {
+      // Return the existing open ticketId
+      return ticket_id.id
+    } else {
+      // return null because I want to create ticket only on first message
       return null
     }
-
-    if (ticket_id && ticket_id.length > 0) {
-      // Return the existing open ticketId
-      return ticket_id[0].id
-    } else {
-      //return value from cookies if no coversationId (I create cookie for coversationId in Layout.tsx)
-      return getCookie("ticketId")?.value
-    }
   } catch (error) {
-    console.error("Error - ", error)
+    console.error(36, "Error - ", error)
     return null
   }
 }
@@ -40,8 +38,8 @@ export default getTicketId
 /** to mark ticket as closed
  const { data, error } = await supabaseAdmin
   .from("tickets")
-  .update({ is_open: true })
-  .eq("sender_id", userSessionData.session?.user.id)
+  .update({ is_open: false })
+  .eq("owner_id", userSessionData.session?.user.id)
   .eq("ticket_id", "your-ticket-id-here")
-  .eq("is_open", false);
+  .eq("is_open", true);
  */
