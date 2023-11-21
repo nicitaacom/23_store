95f313dc9a0a49e07273897c73f9b7734e368da1
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-21 18:32:49 +0100

    +-upd: renamed to getInitialTickets

--- app/actions/getInitialTickets.ts
+++ app/actions/getInitialTickets.ts
@@ -1,7 +1,7 @@
 import { ITicket } from "@/interfaces/ITicket"
 import supabaseAdmin from "@/libs/supabaseAdmin"
 
-const getTickets = async () => {
+const getInitialTickets = async () => {
   const { data: tickets_response, error: tickets_error } = await supabaseAdmin
     .from("tickets")
     .select("*")
@@ -15,4 +15,4 @@ const getTickets = async () => {
   return tickets_response as ITicket[]
 }
 
-export default getTickets
+export default getInitialTickets
