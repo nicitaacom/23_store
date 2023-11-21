cb656046af6917487e7a91f635789ff8ed6a6783
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-21 16:25:57 +0100

    +-upd: renamed to getInitialMessagesByTicketId
    Because its initial messages that I got for server side - it can't be changed dynamicaly - only by location.refresh() - that's why initial messages

--- app/actions/getMessagesByTicketId.ts
+++ app/actions/getMessagesByTicketId.ts
@@ -1,6 +1,6 @@
 import supabaseServer from "@/libs/supabaseServer"
 
-const getMessagesByTicketId = async (ticketId: string) => {
+const getInitialMessagesByTicketId = async (ticketId: string) => {
   const { data: messages_by_id_response, error: messages_by_id_error } = await supabaseServer()
     .from("messages")
     .select("*")
@@ -8,4 +8,4 @@ const getMessagesByTicketId = async (ticketId: string) => {
   if (messages_by_id_error) console.log(8, "messages_by_id_error - ", messages_by_id_error)
   return messages_by_id_response
 }
-export default getMessagesByTicketId
+export default getInitialMessagesByTicketId
