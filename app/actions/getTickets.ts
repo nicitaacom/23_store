e02990bd26431dba161ab41cca72858c92de9453
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-21 12:41:32 +0100

    -upd: get open tickets only

--- app/actions/getTickets.ts
+++ app/actions/getTickets.ts
@@ -2,7 +2,10 @@ import { ITicket } from "@/interfaces/ITicket"
 import supabaseAdmin from "@/libs/supabaseAdmin"
 
 const getTickets = async () => {
-  const { data: tickets_response, error: tickets_error } = await supabaseAdmin.from("tickets").select("*")
+  const { data: tickets_response, error: tickets_error } = await supabaseAdmin
+    .from("tickets")
+    .select("*")
+    .eq("is_open", true)
 
   if (tickets_error) {
     console.log(6, "tickets error - ", tickets_error)
