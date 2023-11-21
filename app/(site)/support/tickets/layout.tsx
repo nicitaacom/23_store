e7687a6e523a2d376fd312676b9d524a0bd98772
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-21 13:55:26 +0100

    +feat: show last message and ticket owner username

--- app/(site)/support/tickets/layout.tsx
+++ app/(site)/support/tickets/layout.tsx
@@ -1,12 +1,16 @@
 import { redirect } from "next/navigation"
+import supabaseServer from "@/libs/supabaseServer"
 
 import getTickets from "@/actions/getTickets"
-import supabaseServer from "@/libs/supabaseServer"
+import getLastMessages from "@/actions/getLastMessages"
 import { DesktopSidebar, MobileSidebar } from "./components"
 
 export default async function SupportChatLayout({ children }: { children: React.ReactNode }) {
   const { data: role_response, error: anonymous_user } = await supabaseServer().from("users").select("role").single()
   const tickets = await getTickets()
+
+  const last_messages = await getLastMessages()
+  // TODO - getAmountOfUnreadMessages()
 
   if (role_response?.role === "USER" || anonymous_user) {
     // boilerplate if you have more roles without access to /support/chat
@@ -20,7 +24,7 @@ export default async function SupportChatLayout({ children }: { children: React.
 
   return (
     <div className="relative h-[calc(100vh-64px)] flex">
-      <DesktopSidebar />
+      <DesktopSidebar tickets={tickets} last_messages={last_messages ?? []} />
       <MobileSidebar tickets={tickets} />
       {children}
     </div>
