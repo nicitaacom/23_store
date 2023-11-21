b78d3fee43b150ac7552cbe387f828676bf19dd6
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-21 16:25:03 +0100

    +-upd: UI for message

--- app/components/SupportButton/components/MessageBox.tsx
+++ app/components/SupportButton/components/MessageBox.tsx
@@ -20,25 +20,28 @@ export function MessageBox({ message }: MessageBoxProps) {
   useEffect(() => {
     //axios.post('/api/messages/seen')
   }, [])
-  if (!message || !message.sender_id) {
-    // Handle the case when message or sender_id is not defined
-    // For example, you can return a placeholder or an error message
-    return <div>Error: Message or sender_id is not defined</div>
-  }
 
-  console.log(19, "message - ", message.sender_id)
+  if (!message || !message.sender_id) {
+    return null
+  }
 
   const userId = userStore.userId === "" ? getCookie("anonymousId") : userStore.userId
   const isOwn = userId === message.sender_id
 
   // TODO - show gray-bg for !isOwn messages
-  const messageIsOwn = twMerge(isOwn ? "bg-info text-title" : "bg-foreground")
+  const messageIsOwn = twMerge(
+    isOwn
+      ? "bg-info"
+      : `before:left-[5px] before:border-l-0 before:border-r-2 before:rounded-r-none before:rounded-b-full
+      before:rotate-[145deg] before:bottom-[-6px] 
+      bg-foreground before:bg-foreground text-title`,
+  )
 
   return (
-    <div className={twMerge(`flex gap-x-2`, isOwn && "justify-end")}>
+    <div className={twMerge(`w-full flex gap-x-2`, isOwn && "justify-end")}>
       <Image
         className={`w-[42px] h-[42px] mt-1 rounded-full select-none pointer-events-none ${
-          isOwn ? "order-first" : "order-last"
+          isOwn ? "order-last" : "order-first"
         }`}
         src={
           userStore.isAuthenticated
@@ -53,17 +56,21 @@ export function MessageBox({ message }: MessageBoxProps) {
         width={46}
         height={46}
       />
-      <div className="relative flex flex-col items-end px-1 py-0.5">
-        <p className="text-xs">{formatTime(message.created_at)}</p>
+      <article
+        className={twMerge("relative max-w-[50%] flex flex-col px-1 py-0.5", isOwn ? "items-end" : "items-start")}>
+        <p className={twMerge("w-full text-xs", isOwn ? "text-end" : "text-start")}>{formatTime(message.created_at)}</p>
         <p
-          className="relative w-fit  border-2 text-start text-title-foreground pl-2 pr-3 pt-0.5 pb-1 bg-info rounded-lg
+          className={twMerge(
+            `relative w-fit max-w-full break-all border-2 text-start text-title-foreground pl-2 pr-3 pt-0.5 pb-1 bg-info rounded-lg
          before:w-3 before:h-3 before:bg-info before:border-l-2 before:border-t-2 before:border-solid before:border-border-color
-       before:rotate-[195deg] before:rounded-r-full before:absolute before:bottom-[-4px] before:right-[-6px] before:translate-x-[-50%]">
+       before:rotate-[195deg] before:rounded-r-full before:absolute before:bottom-[-4px] before:right-[-6px] before:translate-x-[-50%]`,
+            messageIsOwn,
+          )}>
           {message.body}
         </p>
         {/* TODO - seen in the future */}
         {/* <BsCheck2 className="absolute bottom-[2px] right-2 " /> */}
-      </div>
+      </article>
     </div>
   )
 }
