1b65bac4a7efce852b8aea3d3088d3643422b513
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-21 20:06:47 +0100

    style: w-full and flex-col gap-y-2
    Previouly it I saw box-shadow and it had width w-[calc(100%-16rem)] - I don't need it

--- app/(site)/support/tickets/[ticketId]/components/MessagesBody.tsx
+++ app/(site)/support/tickets/[ticketId]/components/MessagesBody.tsx
@@ -52,7 +52,7 @@ export function MessagesBody({ initialMessages, ticket_id }: MessagesBodyProps) 
   if (messages.length === 0) {
     return (
       <main
-        className="w-full h-full laptop:w-[calc(100%-16rem)] hidden laptop:flex bg-foreground-accent
+        className="w-full h-full hidden laptop:flex flex-col gap-y-2 bg-foreground-accent
       justify-center items-center
     shadow-[inset_0px_8px_6px_rgba(0,0,0,0.4)] z-[100]">
         <p>No messages in this ticket</p>
