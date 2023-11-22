8804b10eab12ada348d4a9d87e2cbe1eb6181bbc
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-22 16:08:54 +0100

    +upd: owner_id and owner_avatar_url
    I added this to show image in DesktopSidebar

--- app/interfaces/ITicket.ts
+++ app/interfaces/ITicket.ts
@@ -3,5 +3,7 @@ export interface ITicket {
   created_at: string
   is_open: boolean
   owner_username: string
+  owner_id: string
   last_message_body: string
+  owner_avatar_url: string | undefined
 }
