4603cf171514b674b26ad22cf8dd4f3e77562ed0
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-22 14:23:43 +0100

    +-upd: getInitialMessages marked as unused
    It will be deleted in next major version
    Next major version its when I will done with this task

---
+++ app/actions/getInitialMessages (unused).ts
@@ -0,0 +1,29 @@
+// import supabaseAdmin from "@/libs/supabaseAdmin"
+// import supabaseServer from "@/libs/supabaseServer"
+// import { getCookie } from "@/utils/helpersSSR"
+
+// const getInitialMessages = async () => {
+//   //get userId to fetch messages by userId
+//   const {
+//     data: { user },
+//   } = await supabaseServer().auth.getUser()
+
+//   const userId = user?.id === undefined ? getCookie("anonymousId")?.value : user.id
+
+//   // get messages by userId
+//   const { data: messages, error: messages_error } = await supabaseAdmin // because may be anonymousId that not matches RLS (sender_id = auth.uid())
+//     .from("messages")
+//     .select("*")
+//     .eq("sender_id", userId as string)
+//     .eq("sender_id")
+//   if (messages_error) {
+//     console.log(22, "messages_error - ", messages_error)
+//     throw messages_error
+//   }
+//   if (messages && messages.length === 0) {
+//     return null
+//   }
+//   return messages
+// }
+
+// export default getInitialMessages
