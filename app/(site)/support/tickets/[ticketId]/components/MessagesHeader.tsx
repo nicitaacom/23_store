04e354d0dde46615d1502c55ef567ea63ca58799
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-11-23 11:59:05 +0100

    +-style: rounded-md for image in MessagesHeader

--- app/(site)/support/tickets/[ticketId]/components/MessagesHeader.tsx
+++ app/(site)/support/tickets/[ticketId]/components/MessagesHeader.tsx
@@ -18,7 +18,7 @@ export function MessagesHeader({ owner_username, owner_avatar_url, owner_id }: M
     <div className="w-full shadow-[inset_0px_4px_6px_rgba(0,0,0,0.4)] bg-foreground">
       <div className="flex flex-row gap-x-2 justify-between shadow-[0px_6px_4px_rgba(0,0,0,0.25)] px-4 py-2">
         <div className="flex flex-row gap-x-2 items-center">
-          <Image src={avatar_url} alt="owner_avatar_url" width={32} height={32} />
+          <Image className="rounded-md" src={avatar_url} alt="owner_avatar_url" width={32} height={32} />
           <div className="flex flex-col break-all">
             <h2 className="font-semibold text-sm truncate max-w-[calc(100vw-136px)]">{owner_username}</h2>
             {/* TODO - add active/inactive */}
