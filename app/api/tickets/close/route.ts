542430fd6d3364af21c7df8471e4e4b93a484b1e
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-12-07 16:56:07 +0100

    +chore: comment why I need tickets:closeBySupport

--- app/api/tickets/close/route.ts
+++ app/api/tickets/close/route.ts
@@ -13,11 +13,13 @@ export async function POST(req: Request) {
 
   const { error } = await supabaseAdmin.from("tickets").update({ is_open: false }).eq("id", ticketId)
   if (error) return NextResponse.json({ error: `Error in api/tickets/route.ts\n ${error.message}` }, { status: 400 })
+  // to don't show ticket on support side
   await pusherServer.trigger("tickets", "tickets:close", {
     id: ticketId,
     is_open: false,
   } as ITicket)
-  // separate event is required for action when support close ticket (if you chane event to tickets:close it will not work)
+  // separate event is required for action when support close ticket (if you change event to tickets:close it will not work)
+  // to show on user side 'rate this ticket'
   await pusherServer.trigger(ticketId, "tickets:closeBySupport", null)
 
   return NextResponse.json({ message: "Ticket marked as completed (ticket closed)" }, { status: 200 })
