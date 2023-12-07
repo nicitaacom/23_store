5b9b7a2f211c6753fa2b367e97476a2f00caa92b
Nikita <39565703+nicitaacom@users.noreply.github.com>
2023-12-07 16:58:18 +0100

    fix: pusher trigger deleted
    Because I dpn't need to trigger this event twice because only user may rate this so I don't show messages on support side from first time when user make this request api/tickets/close

--- app/api/tickets/rate/route.ts
+++ app/api/tickets/rate/route.ts
@@ -13,11 +13,7 @@ export async function POST(req: Request) {
   const { ticketId, rate } = (await req.json()) as TAPITicketsRate
   const { error } = await supabaseAdmin.from("tickets").update({ is_open: false, rate: rate }).eq("id", ticketId)
   if (error) return NextResponse.json({ error: `Error in api/tickets/route.ts\n ${error.message}` }, { status: 400 })
-  // to don't show ticket it on support side
-  await pusherServer.trigger("tickets", "tickets:close", {
-    id: ticketId,
-    is_open: false,
-  } as ITicket)
+
   // to clear messages on user side
   await pusherServer.trigger(ticketId, "tickets:close", null)
 
