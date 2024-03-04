42f1a6fa330921bb3c0b8eb0bf0f1022394a29f0
Nikita <nicitaacom@gmail.com>
2024-03-04 15:21:29 +0100

    dynamic for modals

--- app/providers/ModalsProvider.tsx
+++ app/providers/ModalsProvider.tsx
@@ -1,14 +1,35 @@
-"use client"
+import React from "react"
+import dynamic from "next/dynamic"
 
-import { CtrlKModal } from "@/components/ui/Modals/CtrlKModal"
-import {
-  AreYouSureClearCartModal,
-  AreYouSureDeleteProductModal,
-  AreYouSureMarkTicketAsCompletedSupportModal,
-  DoYouWantRecieveCheckModal,
-} from "@/components/ui/Modals"
+const AreYouSureClearCartModal = dynamic(
+  () => import("@/components/ui/Modals/AreYouSureClearCartModal").then(mod => mod.AreYouSureClearCartModal),
+  {
+    loading: () => <div>Loading AreYouSureClearCartModal...</div>,
+  },
+)
 
-//This provider uses only for modals based on useState
+const AreYouSureDeleteProductModal = dynamic(
+  () => import("@/components/ui/Modals/AreYouSureDeleteProductModal").then(modal => modal.AreYouSureDeleteProductModal),
+  { loading: () => <div>Loading AreYouSureDeleteProductModal...</div> },
+)
+
+const AreYouSureMarkTicketAsCompletedSupportModal = dynamic(
+  () =>
+    import("@/components/ui/Modals/AreYouSureMarkTicketAsCompletedSupportModal").then(
+      modal => modal.AreYouSureMarkTicketAsCompletedSupportModal,
+    ),
+  { loading: () => <div>Loading AreYouSureMarkTicketAsCompletedSupportModal...</div> },
+)
+
+const CtrlKModal = dynamic(() => import("@/components/ui/Modals/CtrlKModal").then(modal => modal.CtrlKModal), {
+  loading: () => <div>Loading CtrlKModal...</div>,
+})
+
+const DoYouWantRecieveCheckModal = dynamic(
+  () => import("@/components/ui/Modals/DoYouWantRecieveCheckModal").then(modal => modal.DoYouWantRecieveCheckModal),
+  { loading: () => <div>Loading DoYouWantRecieveCheckModal...</div> },
+)
+
 export function ModalsProvider() {
   return (
     <>
