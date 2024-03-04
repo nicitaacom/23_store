0b8fe22d17075be623b0953f6dd336df6a0f3e4e
Nikita <nicitaacom@gmail.com>
2024-03-04 14:05:10 +0100

    only modals query

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,8 +48,8 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         {children}
         {/* </Layout> */}
         <ModalsQueryProvider ownerProducts={ownerProducts ?? []} />
-        <ModalsProvider />
-        <ToastProvider />
+        {/* <ModalsProvider /> */}
+        {/* <ToastProvider /> */}
         {/* </ClientOnly> */}
       </body>
     </html>
