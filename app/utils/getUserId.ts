10935ad2571f2ba0495bffa9937e094d8e4fa19b
Nikita <nicitaacom@gmail.com>
2026-01-21 13:54:20 +0100

    -console log

--- app/utils/getUserId.ts
+++ app/utils/getUserId.ts
@@ -5,7 +5,6 @@ import { setAnonymousId } from "./setAnonymousId"
 export function getUserId(): string {
   const { userId: userIdStore } = useUserStore.getState()
 
-  console.log(8, "userIdStore - ", userIdStore)
   const userId = userIdStore || getCookie("anonymousId") || setAnonymousId()
 
   return userId
