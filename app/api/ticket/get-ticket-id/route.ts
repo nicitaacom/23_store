1a5482362e1e22525b920217a7662d1ba582d712
Nikita <nicitaacom@gmail.com>
2026-02-21 20:44:23 +0100

    chore: commented line to fix tickets select

--- app/api/ticket/get-ticket-id/route.ts
+++ app/api/ticket/get-ticket-id/route.ts
@@ -17,7 +17,8 @@ export async function POST(req: Request) {
   const { data: ticket_id } = await supabaseAdmin
     .from("tickets")
     .select("id")
-    .eq("owner_username", userId)
+    .eq("owner_username", userId) // TODO - what? WTF? - like owner_username it's owner username but it's defenitely NOT userId
+    // how to even supposed to work with anonymousId and userId - you should have separated logic for that or separated "tickets" tables in DB
     .eq("is_open", true)
     .single()
   if (!ticket_id) {
