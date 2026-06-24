44a5fcadfdb5cb5c37f6de989bbbcbf3ca81fa86
Nikita <nicitaacom@gmail.com>
2026-06-24 17:46:36 +0200

    fix: allow " " space in title

--- app/utils/productValidation.ts
+++ app/utils/productValidation.ts
@@ -1,12 +1,12 @@
 import { MAX_PRODUCT_DESCRIPTION_LENGTH, MAX_PRODUCT_TITLE_LENGTH, MIN_PRODUCT_TITLE_LENGTH } from "@/constants/productLimits"
 
-export const PRODUCT_TITLE_INVALID_CHARACTER_REGEX = /[^A-Za-z0-9#%$()_+&/,.'-]/
+export const PRODUCT_TITLE_INVALID_CHARACTER_REGEX = /[^A-Za-z0-9#%$()_+&/,.'\- |]/
 export const PRODUCT_DESCRIPTION_INVALID_CHARACTER_REGEX = /[^-:.,()#@&%\/"'`~\[\]><=+!?*_;a-zA-Z0-9\n ]/
 export const PRODUCT_TITLE_HAS_LETTER_REGEX = /[A-Za-z]/
 export const PRODUCT_TITLE_MUST_START_REGEX = /^[A-Za-z0-9]/
 
 export const PRODUCT_TITLE_PATTERN = new RegExp(
-  `^(?=.*[A-Za-z])[A-Za-z0-9][A-Za-z0-9 #%$()_+&/,.'-]{${MIN_PRODUCT_TITLE_LENGTH - 1},${MAX_PRODUCT_TITLE_LENGTH - 1}}$`,
+  `^(?=.*[A-Za-z])[A-Za-z0-9][A-Za-z0-9 |#%$()_+&/,.'-]{${MIN_PRODUCT_TITLE_LENGTH - 1},${MAX_PRODUCT_TITLE_LENGTH - 1}}$`,
 )
 
 export const PRODUCT_DESCRIPTION_PATTERN = new RegExp(
