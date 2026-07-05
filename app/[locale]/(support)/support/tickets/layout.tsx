bfe7748984e46590bbbe1363f11907db6272c816
Nikita <nicitaacom@gmail.com>
2026-07-05 09:01:06 +0200

    style: /support fix paddings

--- app/[locale]/(support)/support/tickets/layout.tsx
+++ app/[locale]/(support)/support/tickets/layout.tsx
@@ -29,10 +29,12 @@ export default async function SupportChatLayout({ children }: { children: React.
   return (
     <>
       <ClientOnly>
-        <Navbar />
-        <div className="flex h-[calc(100vh-64px)] min-h-0 bg-background px-2 pb-2 pt-2 laptop:gap-4 laptop:px-4 laptop:pb-4">
-          <SupportTicketsSidebar unseenMessages={unreadMessages ?? []} initialTickets={initialTickets} />
-          <div className="flex min-w-0 flex-1">{children}</div>
+        <div className="pt-16">
+          <Navbar />
+          <div className="flex h-[calc(100vh-64px)] min-h-0 bg-background px-2 pb-2 pt-2 laptop:gap-4 laptop:px-4 laptop:pb-4">
+            <SupportTicketsSidebar unseenMessages={unreadMessages ?? []} initialTickets={initialTickets} />
+            <div className="flex min-w-0 flex-1">{children}</div>
+          </div>
         </div>
       </ClientOnly>
     </>
