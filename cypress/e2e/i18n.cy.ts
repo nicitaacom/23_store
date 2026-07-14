5a417c5bf3bd44747dc5c9c028a61b49e3ad72dd
Nikita <nicitaacom@gmail.com>
2026-07-14 15:47:22 +0200

    chore: stabilize cypress i18n in firefox

--- cypress/e2e/i18n.cy.ts
+++ cypress/e2e/i18n.cy.ts
@@ -1,8 +1,8 @@
 import english from "../../app/locales/en"
 import finnish from "../../app/locales/fi"
+import { formatCurrency } from "../../app/utils/currencyFormatter"
 import russian from "../../app/locales/ru"
 import swedish from "../../app/locales/se"
-import { formatCurrency } from "../../app/utils/currencyFormatter"
 
 function getTranslationKeys(value: unknown, prefix = ""): string[] {
   if (!value || typeof value !== "object" || Array.isArray(value)) return [prefix]
@@ -31,16 +31,19 @@ describe("internationalization", () => {
 
   it("persists a selected language after navigation and refresh", () => {
     cy.intercept("POST", "/api/messages/get-messages").as("getMessages")
+    cy.intercept("POST", "/").as("trackVisit")
     cy.visit("/")
-    cy.wait("@getMessages")
+    cy.wait(["@getMessages", "@trackVisit"])
 
     cy.get('[data-cy="language-trigger"]').first().click()
     cy.get('[data-cy="language-en"]').first().click()
 
     cy.getCookie("Next-Locale").should("have.property", "value", "en")
     cy.get('[data-cy="language-trigger"]').first().should("contain.text", "en")
+    cy.wait("@trackVisit")
 
     cy.reload()
+    cy.wait("@trackVisit")
     cy.get('[data-cy="language-trigger"]').first().should("contain.text", "en")
     cy.document().its("documentElement.lang").should("equal", "en")
   })
