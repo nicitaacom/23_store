f1376c7a1f15a0b8cec141d82c9ac5b43c71051f
Nikita <nicitaacom@gmail.com>
2026-08-06 11:42:18 +0200

    fix: add supbase in next.js.config
    cuz it still reads prev 3rd DB broken img
    cuz I not deleted it

--- next.config.js
+++ next.config.js
@@ -29,6 +29,11 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "sckioxanzluozlghvnts.supabase.co",
+        port: "",
+      },
+      {
+        protocol: "https",
         hostname: "lh3.googleusercontent.com",
         port: "",
       },
